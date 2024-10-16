2ef8e56cb7e5a496e677c41256549122a46a95a8
pkgu-odoo <pkgu@odoo.com>
2024-10-16 15:01:20 +0530

    [IMP] wscs_vendor_extension:  Refactor code to eliminate unnecessary lines

--- wscs_vendor_extension/models/purches_order.py
+++ wscs_vendor_extension/models/purches_order.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api, _
+from odoo import models, fields, api
 from odoo.exceptions import UserError
 
 class PurchaseOrder(models.Model):
@@ -9,33 +9,29 @@ class PurchaseOrder(models.Model):
         partner_id = vals.get('partner_id')
         if partner_id:
             partner = self.env['res.partner'].browse(partner_id)
+            notify_user = partner.status_id.notify_user_id
             if  partner.status_id and partner.status_id.name in ['Verified - Grade 1', 'Verified - Grade 2']:
                 status_record = self.env['vendor.status'].search([('name', '=', 'Active')], limit=1)
                 partner.status_id = status_record
-                purchase_order = super().create(vals)
-                return purchase_order
 
             elif partner.status_id.prevent_po_creation in ['no'] and partner.status_id and partner.status_id.name in ['Verified - Grade 1', 'Verified - Grade 2']:
                 status_record = self.env['vendor.status'].search([('name', '=', 'Active')], limit=1)
                 partner.status_id = status_record
-                purchase_order = super().create(vals)
-                return purchase_order
 
             elif partner.status_id.prevent_po_creation in ['alert']:
                 status_record = self.env['vendor.status'].search([('name', '=', 'Active')], limit=1)
                 partner.status_id = status_record
                 purchase_order = super().create(vals)
-                notify_user = partner.status_id.notify_user_id
                 if notify_user:
-                    activity_type_id = self.env.ref('mail.mail_activity_data_todo').id
+                    active_type_id = self.env.ref('mail.mail_activity_data_todo').id
                     purchase_order.activity_schedule(
-                    activity_type_id=activity_type_id,
-                    summary=_("Follow up on the newly created Purchase Order"),
-                    note=_("The supplier status has been changed to 'Active'. Please review the purchase order."),
+                    activity_type_id=active_type_id,
+                    summary=("Follow up on the newly created Purchase Order"),
+                    note=("The supplier status has been changed to 'Active'. Please review the purchase order."),
                     user_id=notify_user.id,
                     )
                 return purchase_order
 
             elif partner.status_id.prevent_po_creation in ['yes']:
-                raise UserError(_("You cannot create a Purchase Order because purchase order is already created"))
+                raise UserError("You cannot create a Purchase Order because purchase order is already created")
         return super().create(vals)
